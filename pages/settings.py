45908a99ff2a4f92925c489d286c369c26c3a0ae
Stuart McNeill <stuart.mcneill17@gmail.com>
2013-07-02 18:38:13 +0100

    added taggit_autosuggest to pages.settings

--- pages/settings.py
+++ pages/settings.py
@@ -57,8 +57,9 @@ def get_page_templates():
 # Set ``PAGE_TAGGING`` to ``False`` if you do not wish to use the
 # ``django-taggit application.
 PAGE_TAGGING = getattr(settings, 'PAGE_TAGGING', False)
-if PAGE_TAGGING and "taggit" not in getattr(settings, 'INSTALLED_APPS', []):
-    raise ImproperlyConfigured('django-taggit could not be found.\n'
+# if PAGE_TAGGING and "taggit" not in getattr(settings, 'INSTALLED_APPS', []):
+if PAGE_TAGGING and "taggit_autosuggest" not in getattr(settings, 'INSTALLED_APPS', []):
+    raise ImproperlyConfigured('django-taggit_autosuggest could not be found.\n'
                                'Please make sure you\'ve installed it '
                                'correctly or disable the tagging feature by '
                                'setting PAGE_TAGGING to False.')
